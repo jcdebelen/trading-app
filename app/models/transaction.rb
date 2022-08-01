e7ccd7af15b625dc32ffc5ba06f1490a206f6669
Freigth <cosiconeal@gmail.com>
2022-08-01 22:04:04 +0800

    change transaction table "type" to "action"

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,10 @@
 class Transaction < ApplicationRecord
     validates :user_id, presence: true
-    validates :type, presence: true
+    validates :action, presence: true
     validates :status, presence: true
     validates :amount, presence: true
 
     has_one :stock, dependent: :destroy
     belongs_to :user
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_122241) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_134627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,11 +24,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_122241) do
 
   create_table "transactions", force: :cascade do |t|
     t.integer "user_id"
-    t.string "type"
     t.string "status"
     t.integer "amount", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "action"
   end
 
   create_table "users", force: :cascade do |t|
