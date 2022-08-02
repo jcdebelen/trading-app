4ffc4485e443249cce33204c3b51bb4739e783fc
Freigth <cosiconeal@gmail.com>
2022-08-02 22:28:19 +0800

    admin can create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
+  def admin_new
     @user = User.new
   end
 
-  def create
+  def admin_create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to users_path, notice: "User was successfully created."}
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :admin_new, status: :unprocessable_entity }
       end
     end
   end
