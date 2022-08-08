3c561d217f5c13938579695a6686ef432fb59d59
Freigth <cosiconeal@gmail.com>
2022-08-08 23:48:44 +0800

    add stock symbols on seeds.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    client = IEX::Api::Client.new
+    @symbols = client.ref_data_symbols()
   end
   
 end
