ce0013283077cfb4fb3fc5b280d32dcd4f43223e
Freigth <cosiconeal@gmail.com>
2022-08-15 17:41:16 +0800

    bug on history from string to decimal

--- app/controllers/user_wallet_controller.rb
+++ app/controllers/user_wallet_controller.rb
@@ -11,13 +11,13 @@ class UserWalletController < ApplicationController
         if params[:withdraw].to_f > current_user.balance.to_f
           format.html { redirect_to root_path, notice: "Insufficient balance"}
         else
-          current_user.user_histories.create(status: params[:commit], amount: params[:withdraw])
+          current_user.user_histories.create(status: params[:commit], amount: params[:withdraw].to_f)
           current_user.balance -= params[:withdraw].to_f 
           format.html { redirect_to root_path, notice: "You've successfully withdraw $#{params[:withdraw]}"}
           current_user.save
         end
       elsif params[:deposit].present?
-        current_user.user_histories.create(status: params[:commit], amount: params[:deposit])
+        current_user.user_histories.create(status: params[:commit], amount: params[:deposit].to_f)
         current_user.balance += params[:deposit].to_f 
         format.html { redirect_to root_path, notice: "You've successfully deposited $#{params[:deposit]}"}
         current_user.save
